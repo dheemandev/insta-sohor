96162beba579747cd3f1a5f00f329903dc6e348c
dheemandev <dheemanchakma04@gmail.com>
2022-03-14 20:49:21 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -128,7 +128,7 @@ const createPost = (post) => {
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture"/>
                     </a>
 
                     <span>Liked by
